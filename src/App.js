15ff359f3bb9bdfb8e4a8aef65677d04d5f08d2e
fora-heims <foraheims@gmail.com>
2021-12-06 13:53:59 -0800

    connected to supabase 'blogs' database

--- src/App.js
+++ src/App.js
@@ -1,9 +1,20 @@
-import React from 'react';
+import React, { useState } from 'react';
 import './App.css';
+import { useEffect } from 'react';
 import Header from './components/layout/Header/Header';
 import Footer from './components/layout/Footer/Footer';
+import { client, checkError } from './services/client.js';
+import { getBlogs } from './services/blogs.js';
 
 function App() {
+  const [blogs, setBlogs] = useState([]);
+  useEffect(() => {
+    const fetchData = async () => {
+      const data = await getBlogs();
+      console.log(data);
+    };
+    fetchData();
+  }, []);
   return (
     <section className="main">
       <Header />
