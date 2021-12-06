6994a73f4bb539b5d99b5b60dd6fbbe59eab8c77
fora-heims <foraheims@gmail.com>
2021-12-06 14:43:42 -0800

    mapping a BlogCard component for each blog of blogs

--- src/App.js
+++ src/App.js
@@ -5,19 +5,23 @@ import Header from './components/layout/Header/Header';
 import Footer from './components/layout/Footer/Footer';
 import { client, checkError } from './services/client.js';
 import { getBlogs } from './services/blogs.js';
+import BlogCard from './components/BlogCard/BlogCard.js';
 
 function App() {
   const [blogs, setBlogs] = useState([]);
   useEffect(() => {
     const fetchData = async () => {
       const data = await getBlogs();
-      console.log(data);
+      setBlogs(data);
     };
     fetchData();
   }, []);
   return (
     <section className="main">
       <Header />
+      {blogs.map((blog) => (
+        <BlogCard {...blog} key={blog.id} />
+      ))}
       <Footer />
     </section>
   );
